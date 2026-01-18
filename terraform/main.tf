788712c1bf2000f4080298b5d236a7410aadc2d0
wakinotake <ke.takenowa@gmail.com>
2026-01-18 17:31:25 +0900

    20260118_APP

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,118 @@
-terraform {
-    backend "s3" {
-        bucket = "terraform-state-bucket-20260118-t" # 事前に手動で作ったS3バケット名
-        key    = "infrastructure.tfstate"  # バケット内での保存パス（好きな名前でOK）
-        region = "ap-northeast-1"
-    }
-}
-
 provider "aws" {
   region = "ap-northeast-1"
 }
 
-# DynamoDBテーブルの定義
-resource "aws_dynamodb_table" "example_table" {
-  name           = "example-github-actions-table"
-  billing_mode   = "PAY_PER_REQUEST" # 従量課金（無料枠内での試行に最適）
-  hash_key       = "UserId"
+####################
+# DynamoDB
+####################
+resource "aws_dynamodb_table" "sample" {
+  name         = "sample-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
 
   attribute {
-    name = "UserId"
+    name = "id"
     type = "S"
   }
+}
 
-  tags = {
-    Environment = "Dev"
-    CreatedBy   = "GitHubActions"
+####################
+# IAM Role for Lambda
+####################
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-dynamodb-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name = "lambda-dynamodb-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["dynamodb:PutItem"]
+        Resource = aws_dynamodb_table.sample.arn
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+####################
+# Lambda
+####################
+resource "aws_lambda_function" "api_lambda" {
+  function_name = "api-post-lambda"
+  role          = aws_iam_role.lambda_role.arn
+  runtime       = "python3.12"
+  handler       = "index.handler"
+
+  filename         = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.sample.name
+    }
   }
+}
+
+####################
+# API Gateway (HTTP API)
+####################
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "sample-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.api_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "post_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /items"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+####################
+# Lambda Permission
+####################
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
---
+++ terraform_temp/20260118_初回/main.tf
@@ -0,0 +1,28 @@
+terraform {
+    backend "s3" {
+        bucket = "terraform-state-bucket-20260118-t" # 事前に手動で作ったS3バケット名
+        key    = "infrastructure.tfstate"  # バケット内での保存パス（好きな名前でOK）
+        region = "ap-northeast-1"
+    }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# DynamoDBテーブルの定義
+resource "aws_dynamodb_table" "example_table" {
+  name           = "example-github-actions-table"
+  billing_mode   = "PAY_PER_REQUEST" # 従量課金（無料枠内での試行に最適）
+  hash_key       = "UserId"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  tags = {
+    Environment = "Dev"
+    CreatedBy   = "GitHubActions"
+  }
+}
